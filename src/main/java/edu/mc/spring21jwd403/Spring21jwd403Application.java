062029d5bafab4b7e83f503c691aa892339dcc06
craigh210 <craighunt210@gmail.com>
2021-04-02 12:32:03 -0400

    Initial commit

---
+++ src/main/java/edu/mc/spring21jwd403/Spring21jwd403Application.java
@@ -0,0 +1,13 @@
+package edu.mc.spring21jwd403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring21jwd403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring21jwd403Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mc/spring21jwd403/Spring21jwd403ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.spring21jwd403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring21jwd403ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
